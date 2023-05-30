2614678c05e35f5361f1eda9d269749d98a8269a
Dmitriy Komarov <d.komarov2019@gmail.com>
2023-05-30 13:58:50 +0300

    Меню добавил

---
+++ Assets/MenuScropt.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuScropt : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip audioClip;
+
+    public void Start()
+    {
+        audioSource.clip = audioClip;
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+
+    public void Update()
+    {
+
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
